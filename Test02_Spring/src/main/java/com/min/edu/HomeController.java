664d1b9103042aa627bc2167623371094c688f74
Kimsuyeub <yubk95@naver.com>
2023-09-11 12:31:48 +0900

    test

---
+++ Test02_Spring/src/main/java/com/min/edu/HomeController.java
@@ -0,0 +1,22 @@
+package com.min.edu;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+
+	private static Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping(value = "/home.do",method = RequestMethod.GET)
+	public String home(Model model) {
+		logger.info("Welcome home","home");
+		model.addAttribute("hello","헬로우");
+		return "home";
+	}
+	
+}
---
+++ Test02_Spring/src/test/java/com/test/edu/Test02_JUnitTest.java
@@ -0,0 +1,27 @@
+package com.test.edu;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/**/*.xml"})
+public class Test02_JUnitTest {
+
+	@Autowired
+	private ApplicationContext context;
+	
+	
+	@Test
+	public void test() {
+	 SqlSessionTemplate session =context.getBean("sqlSessionTemplate",SqlSessionTemplate.class);
+	 assertNotNull(session);
+	}
+
+}
